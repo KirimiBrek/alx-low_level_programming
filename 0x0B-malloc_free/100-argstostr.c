3838b20d07663673e9a9c7f764ea34897064f7cc
KirimiBrek <brendakinya195@gmail.com>
2022-04-05 00:36:30 -0700

    hurry

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -26,36 +26,35 @@ int _strlen(char *s)
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
-	char *str;
+	char *dest;
+	unsigned int size, i, j, k;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
-	}
-
-	str = malloc(sizeof(char) * (len + 1));
-
-	if (str == NULL)
-		return (NULL);
-
+	size = 0;
 	k = 0;
 
-	for (i = 0; i < ac; i++)
+	if (ac <= 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < (unsigned int)ac; i++)
+	{
+		size += _strlen(av[i]);
+	}
+
+	dest = (char *)malloc((size + 1) * sizeof(char));
+
+	if (dest == NULL)
+		return (NULL);
+
+	for (i = 0; i < (unsigned int)ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			str[k] = av[i][j];
+			dest[k] = av[i][j];
 			k++;
 		}
-		str[k] = '\n';
+		dest[k] = '\n';
 		k++;
 	}
-
-	return (str);
+	dest[k] = '\0';
+	return (dest);
 }
